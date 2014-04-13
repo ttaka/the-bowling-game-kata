4f3b852e8b9155875acc234e8447ab157ba47d3a
Takashi Takahashi <ttaka.tmp@gmail.com>
2014-04-13 12:11:57 +0900

    The first test.

---
+++ php/kata/src/Game.php
@@ -0,0 +1,12 @@
+<?php
+class Game
+{
+    public function roll($pins)
+    {
+    }
+
+    public function score()
+    {
+        return 0;
+    }
+}
--- php/kata/tests/BowlingGameTest.php
+++ php/kata/tests/BowlingGameTest.php
@@ -1,4 +1,12 @@
 <?php
 class BowlingGameTest extends PHPUnit_Framework_TestCase
 {
+    public function testGutterGame()
+    {
+        $g = new Game();
+        for ($i = 0; $i < 20; $i++) {
+            $g->roll(0);
+        }
+        $this->assertEquals(0, $g->score());
+    }
 }
